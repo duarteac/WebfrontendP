3c405600563316287b096dd11fcec74ef850d9d0
AndresD <andresd@pop-os.localdomain>
2019-10-31 20:48:20 -0500

    profile

--- src/Components/private/index.js
+++ src/Components/private/index.js
@@ -2,34 +2,62 @@ import React from 'react';
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
 import {update} from './../../services/fire';
- 
-
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from '../theme';
+import{addImage} from './../../services/fire';
 function Private(props) {
 
   const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
+
 
   const signout = () => {
     props.setAuthentication(false);
   }
   const updateuser= () =>{
     var id = sessionStorage.getItem('user');
-    update(id,email, name)
+    update(id, name)
     console.log("update succes")
+  }
+  
+  function previewFile() {
+    var preview = document.querySelector('img');
+    var fileloaded   = document.querySelector('input[type=file]');
+    
+    if(file== null ){
+      return null ;
+    }
+    var file = fileloaded.files[0];
+    var url = addImage("perfil/"+sessionStorage.getItem("user"),file);
+
+    var reader  = new FileReader();
+
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+    return reader;
   }
 
   return (
-    <div>
-      <h1>Private</h1>
-      <Button onClick={ updateuser }>actualizar usuario</Button>
-      <TextField defaultValue="nombre"
-      onChange = {e => setName(e.target.value)}
-      />
-      <TextField defaultValue="correo"
-      onChange = {e => setEmail(e.target.value)}
-      />
-      <Button onClick={signout}>Logout</Button>
-    </div>
+    <React.Fragment>
+      <MuiThemeProvider theme ={theme}>
+  
+          <h1>Private</h1>
+          <input type="file" onChange= {event =>previewFile()}></input>
+         <img src="" height="200" alt="Image preview..."></img>
+          <Button onClick={ updateuser }>actualizar usuario</Button>
+          <TextField defaultValue="nombre"
+          onChange = {e => setName(e.target.value)}
+          />
+          
+          <Button onClick={signout}>Logout</Button>
+      </MuiThemeProvider>
+    </React.Fragment>
   );
 }
 
